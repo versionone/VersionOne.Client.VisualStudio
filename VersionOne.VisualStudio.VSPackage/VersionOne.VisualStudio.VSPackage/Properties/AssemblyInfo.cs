f9f8f5eb486455954e17a76391e7a77fd1229968
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-08-05 12:37:29 +0000

    assembly signing is ok now (temp keys are used for 3rd party code, V1 key - for our assemblies)
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/VersionOne.VisualStudio2010@39018 542498e7-56f8-f544-a0f2-a85913ac812a

--- VersionOne.VisualStudio.VSPackage/VersionOne.VisualStudio.VSPackage/Properties/AssemblyInfo.cs
+++ VersionOne.VisualStudio.VSPackage/VersionOne.VisualStudio.VSPackage/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-// TODO
+// TODO remove?
 [assembly: InternalsVisibleTo("VersionOne.VisualStudio.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f33b1033203ca9ff9ce73372ac29a743c997d40a51056e6a7525e08636a07746991f957dc95e4d74523b6b3573e169e0136d53ee4ef8c0ff218583e330633afcdd3ee03dc0b1dbab12ce2d6f4045250e59ea2074e8ab52897b2b27ea4f9e62bf7cde46c16f24f15f014fd4386998b64ef88ac427d00cb777723bdc196d9763a2")]
