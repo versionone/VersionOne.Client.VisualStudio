cb6d90cd3a6f077bcfe474a6f0c3a8531da0f63c
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-10-13 16:02:25 +0000

    background task runner component draft
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/VersionOne.VisualStudio2010@42317 542498e7-56f8-f544-a0f2-a85913ac812a

---
+++ VersionOne.VisualStudio.VSPackage/VersionOne.VisualStudio.VSPackage/BackgroundTaskRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace VersionOne.VisualStudio.VSPackage {
+    /// <summary>
+    /// A wrapper that simplifies execution of long running tasks in background thread so that UI would not be frozen.
+    /// </summary>
+    public partial class BackgroundTaskRunner : Component {
+        public BackgroundTaskRunner() {
+            InitializeComponent();
+        }
+
+        public BackgroundTaskRunner(IContainer container) : this() {
+            container.Add(this);
+        }
+
+        // TODO in most complex cases, we may need separate actions for: task itself, completion, error handling, successful completion
+        public void Run(Action task, Action onComplete, Action onError = null) {
+            worker.DoWork += (sender, e) => task.Invoke();
+            worker.RunWorkerCompleted += (sender, e) => {
+                                             onComplete.Invoke();
+
+                                             if (e.Error != null && onError != null) {
+                                                 onError.Invoke();
+                                             }
+                                         };
+
+            worker.RunWorkerAsync();
+        }
+    }
+}
