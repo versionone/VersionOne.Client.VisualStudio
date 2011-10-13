3b8369153881aff335bcb0f67974af2472b2b649
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-10-13 19:49:22 +0000

    UI support for waiting, currently not suitable (but here's an idea)
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/VersionOne.VisualStudio2010@42319 542498e7-56f8-f544-a0f2-a85913ac812a

---
+++ VersionOne.VisualStudio.VSPackage/VersionOne.VisualStudio.VSPackage/WaitCursor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VersionOne.VisualStudio.VSPackage {
+    public class WaitCursor : IDisposable {
+        private readonly Control control;
+
+        public WaitCursor(Control control) {
+            this.control = control;
+        }
+
+        public IDisposable Show() {
+            control.Cursor = Cursors.WaitCursor;
+            control.Enabled = false;
+            return this;
+        }
+
+        public void Dispose() {
+            control.Cursor = Cursors.Default;
+            control.Enabled = true;
+        }
+    }
+}
