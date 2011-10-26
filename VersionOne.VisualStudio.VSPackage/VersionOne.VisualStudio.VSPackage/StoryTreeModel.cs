6c9fcba6aae86f21b7b1720fabe7eba307cb9959
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-10-26 09:15:24 +0000

    model will not call DataLayer anymore
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/VersionOne.VisualStudio2010@42368 542498e7-56f8-f544-a0f2-a85913ac812a

--- VersionOne.VisualStudio.VSPackage/VersionOne.VisualStudio.VSPackage/StoryTreeModel.cs
+++ VersionOne.VisualStudio.VSPackage/VersionOne.VisualStudio.VSPackage/StoryTreeModel.cs
@@ -3,44 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Aga.Controls.Tree;
-
-using VersionOne.VisualStudio.DataLayer;
 using VersionOne.VisualStudio.DataLayer.Entities;
+using VersionOne.VisualStudio.VSPackage.Controllers;
 using VersionOne.VisualStudio.VSPackage.Descriptors;
 using VersionOne.VisualStudio.VSPackage.Events;
 using VersionOne.VisualStudio.VSPackage.Settings;
 
 namespace VersionOne.VisualStudio.VSPackage {
-    // warnings of unused events are unnecessary here because events are required to implement interface
-    #pragma warning disable 67
-    
     public class StoryTreeModel : ITreeModel {
-        private readonly IDataLayer dataLayer;
+        private readonly WorkitemTreeController controller;
 
-        public StoryTreeModel(IDataLayer dataLayer) {
-            this.dataLayer = dataLayer;
+        public StoryTreeModel(WorkitemTreeController controller) {
+            this.controller = controller;
         }
 
         public IEnumerable GetChildren(TreePath treePath) {
-            if (!dataLayer.IsConnected) {
+            if (!controller.CanRetrieveData) {
                 return null;
             }
 
             if (treePath.IsEmpty()) {
-                try {
-                    return WrapWorkitems(dataLayer.GetWorkitems());
-                } catch (DataLayerException) { 
-                    //TODO show message?
-                }
-            } else {
-                var descriptor = (WorkitemDescriptor) treePath.LastNode;
-                return WrapWorkitems(descriptor.Workitem.Children);
+                var workitems = controller.GetWorkitems();
+                return workitems != null ? WrapWorkitems(workitems) : new WorkitemDescriptor[0];
             }
-            return new WorkitemDescriptor[0];
+
+            var descriptor = (WorkitemDescriptor) treePath.LastNode;
+            return WrapWorkitems(descriptor.Workitem.Children);
         }
 
         // TODO support primary items
-        private List<WorkitemDescriptor> WrapWorkitems(IList<Workitem> workitems) {
+        private static IEnumerable<WorkitemDescriptor> WrapWorkitems(ICollection<Workitem> workitems) {
             var items = new List<WorkitemDescriptor>(workitems.Count);
             items.AddRange(workitems.Select(workitem => new WorkitemDescriptor(workitem, Configuration.Instance.GridSettings.Columns, PropertyUpdateSource.WorkitemView, false)));
 
@@ -52,15 +44,17 @@ namespace VersionOne.VisualStudio.VSPackage {
             return descriptor.Workitem.Children.Count == 0;
         }
 
+#pragma warning disable 67
+
         public event EventHandler<TreeModelEventArgs> NodesChanged;
         public event EventHandler<TreeModelEventArgs> NodesInserted;
         public event EventHandler<TreeModelEventArgs> NodesRemoved;
         public event EventHandler<TreePathEventArgs> StructureChanged;
 
+#pragma warning restore 67
+
         public void InvokeStructureChanged(){
             StructureChanged(this, new TreePathEventArgs());
         }
     }
-
-    #pragma warning restore 67
 }
