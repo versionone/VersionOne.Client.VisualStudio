e7afd99dec551333d4ba075d2356232eb637cee2
jogoshugh <jgough@JGough.corp.versionone.net>
2013-09-23 08:55:00 -0400

    Moved FindByMather code to a separate extensions file.

---
+++ VersionOne.VisualStudio.VSPackage/Controls/AdvTreeViewExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Aga.Controls.Tree;
+
+namespace VersionOne.VisualStudio.VSPackage.Controls
+{
+  public static class AdvTreeViewExtensions
+  {
+    public static TreeNodeAdv FindNodeByMather(this TreeViewAdv node, Predicate<TreeNodeAdv> matcher)
+    {
+      return FindNodeByMather(node.Root, matcher);
+    }
+
+    private static TreeNodeAdv FindNodeByMather(TreeNodeAdv root, Predicate<TreeNodeAdv> matcher)
+    {
+      foreach (var adv in root.Children)
+      {
+        if (matcher(adv))
+        {
+          return adv;
+        }
+        var adv2 = FindNodeByMather(adv, matcher);
+        if (adv2 != null)
+        {
+          return adv2;
+        }
+      }
+      return null;
+    }
+  }
+}
--- VersionOne.VisualStudio.VSPackage/Controls/WorkitemTreeControl.cs
+++ VersionOne.VisualStudio.VSPackage/Controls/WorkitemTreeControl.cs
@@ -419,28 +419,4 @@ namespace VersionOne.VisualStudio.VSPackage.Controls {
             }
         }
     }
-    public static class AdvTreeViewExtensions
-    {
-        public static TreeNodeAdv FindNodeByMather(this TreeViewAdv node, Predicate<TreeNodeAdv> matcher)
-        {
-            return FindNodeByMather(node.Root, matcher);
-        }
-
-        private static TreeNodeAdv FindNodeByMather(TreeNodeAdv root, Predicate<TreeNodeAdv> matcher)
-        {
-            foreach (var adv in root.Children)
-            {
-                if (matcher(adv))
-                {
-                    return adv;
-                }
-                var adv2 = FindNodeByMather(adv, matcher);
-                if (adv2 != null)
-                {
-                    return adv2;
-                }
-            }
-            return null;
-        }
-    }
 }
