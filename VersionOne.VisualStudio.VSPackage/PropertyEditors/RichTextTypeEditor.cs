acc4550d429776e7b21da16e93ee8b42bae0aa14
MatiasHeffel <matiasheffel@hotmail.com>
2013-12-03 17:47:57 -0300

    Issue #75 display plain text

--- VersionOne.VisualStudio.VSPackage/PropertyEditors/RichTextTypeEditor.cs
+++ VersionOne.VisualStudio.VSPackage/PropertyEditors/RichTextTypeEditor.cs
@@ -1,33 +1,42 @@
 using System;
 using System.ComponentModel;
+using System.Drawing;
 using System.Drawing.Design;
 using System.Windows.Forms;
 using System.Windows.Forms.Design;
-
+using System.Windows.Media;
 using VersionOne.VisualStudio.VSPackage.Forms;
 
-namespace VersionOne.VisualStudio.VSPackage.PropertyEditors {
-    public class RichTextTypeEditor : UITypeEditor {
-        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context) {
+namespace VersionOne.VisualStudio.VSPackage.PropertyEditors
+{
+    public class RichTextTypeEditor : UITypeEditor
+    {
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
             return UITypeEditorEditStyle.Modal;
         }
 
-        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value) {
-            var editorService = (IWindowsFormsEditorService) provider.GetService(typeof(IWindowsFormsEditorService));
-            
-            if (editorService == null) {
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            var editorService = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+
+            if (editorService == null)
+            {
                 return base.EditValue(context, provider, value);
             }
 
-            var modalEditor = new RichTextEditorDialog {
-                                      HtmlData = (value == null) ? string.Empty : (string) value,
-                                  };
+            string valueWithHtmlTags = ((VSPackage.Descriptors.WorkitemPropertyDescriptor)context.PropertyDescriptor).GetDescriptionRow();
+            var modalEditor = new RichTextEditorDialog
+            {
+                HtmlData = valueWithHtmlTags,
+            };
 
-            if (editorService.ShowDialog(modalEditor) == DialogResult.OK) {
+            if (editorService.ShowDialog(modalEditor) == DialogResult.OK)
+            {
                 return modalEditor.HtmlData != "<P>&nbsp;</P>" ? modalEditor.HtmlData : "<br>";
             }
-            
+
             return value;
-        }
+        }    
     }
 }
